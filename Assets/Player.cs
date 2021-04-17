206a15e9942aa361cd8525152d0b01c201c00334
Daniel Iova <iovadaniel01@gmail.com>
2021-04-17 17:49:05 +0300

    Added random sprite changing functionality. Must add fire and water sprites.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,12 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Elements and their respective CircleCollider radius
+    private readonly string[] elements = {"Air", "Earth" };
+    private readonly float[] elementColliderRadius = { 5f, 5.8f };
+
     public float jumpForce = 10f;
-    // Start is called before the first frame update
-    /*void Start()
+
+    public CircleCollider2D circleCollider;
+
+    private SpriteRenderer spriteRenderer;
+
+    private int currentElementIndex = -1;
+
+    void ChangeSprite()
     {
+        currentElementIndex = Random.Range(0, elements.Length);
+        spriteRenderer.sprite = Resources.Load<Sprite>($"Sprites/player{elements[currentElementIndex]}");
+        // Change collider radius based on element
+        circleCollider.radius = elementColliderRadius[currentElementIndex];
+    }
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
         
-    }*/
+        ChangeSprite();
+    }
 
     public Rigidbody2D rb;
 
